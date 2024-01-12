862cd020fbafab2c8d992bba206eaf23c186e1b8
Al Macleod Stage <al@peeldev.com>
2024-01-11 20:32:09 -0800

    rename

--- peel/net/udp.cpp
+++ peel/net/udp.cpp
@@ -19,10 +19,23 @@ bool Udp::Create()
 size_t Udp::GetDatagram(char* buf, int len, Address& m_from)
 {
 	socklen_t l = sizeof(struct sockaddr_in);
-	size_t ret = recvfrom(m_socket, buf, len, 0, m_from, &l);
+	int ret = recvfrom(m_socket, buf, len, 0, m_from, &l);
 	if (WSAGetLastError() == WSAEWOULDBLOCK)
 		return 0;
 	if (ret == (size_t)-1) throw Error("Error getting datagram", GetError());
 	buf[ret] = '\0';
 	return ret;
+}
+
+void Udp::SendDatagram(const char* buf, int len, const Address& to)
+{
+	int ret = sendto(m_socket, buf, len, 0, (sockaddr*)(&to.m_address), sizeof(sockaddr));
+	if (WSAGetLastError() == WSAEWOULDBLOCK) {
+		return;
+	}
+
+	if (ret == SOCKET_ERROR) {
+		throw Error("Error sending datagram", GetError());
+	}
+
 }
--- peel/net/udp.h
+++ peel/net/udp.h
@@ -16,6 +16,7 @@ namespace peel
 			Udp();
 			virtual bool Create() override;
 			size_t GetDatagram(char* msg, int maxLen, Address& m_from);
+			void   SendDatagram(const char* msg, int len, const Address& to);
 		};
 	} // socket
 } // peel
